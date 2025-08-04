63c02b70dcd7eea79e174048447cbd0f126fdd2b
Adam Tovatt <sporedenstore@hotmail.com>
2025-08-04 13:52:13 +0200

    include install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Variables
+URL="https://github.com/AdamTovatt/NetlifyDnsManager/releases/download/v1.0.0/linux-arm64.zip"
+DEST_DIR="/opt/netlify-dns-manager"
+SERVICE_FILE="/etc/systemd/system/netlify-dns-manager.service"
+ZIP_FILE="/tmp/netlify-dns-manager.zip"
+USER_NAME=$(whoami)
+
+# Download and unzip
+curl -L "$URL" -o "$ZIP_FILE"
+mkdir -p "$DEST_DIR"
+unzip -o "$ZIP_FILE" -d "$DEST_DIR"
+chmod +x "$DEST_DIR/NetlifyDnsManager"
+
+# Create systemd service file
+cat <<EOF > "$SERVICE_FILE"
+[Unit]
+Description=Netlify Dns Manager Background Service
+
+[Service]
+ExecStart=$DEST_DIR/NetlifyDnsManager
+User=$USER_NAME
+Restart=always
+RestartSec=4
+
+Environment=NETLIFY_ACCESS_TOKEN=[your-token]
+Environment=DOMAIN_01=[your-first-domain]
+Environment=CHECK_INTERVAL=[your-desired-check-interval-in-seconds]
+Environment=ENABLE_LOGGING=[true/false-to-determine-if-it-should-log-all-information]
+
+[Install]
+WantedBy=multi-user.target
+EOF
